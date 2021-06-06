4d0d60b4f74713633ef30681df21ed52b4bfb50e
Alanna <croysdaa@oregonstate.edu>
2021-06-06 14:14:58 -0700

    Added tests

---
+++ test.py
@@ -0,0 +1,10 @@
+import example
+import unittest
+
+class TestCase (unittest.TestCase):
+    
+    def test_add_1 (self):
+        self.assertEqual(example.add(1,2), 3)
+
+if __name__ == '__main__:
+    unitest.main()
