bedf5b1833fc77782637a67dc94b4e03666c7b71
Alanna <croysdaa@oregonstate.edu>
2021-06-06 22:47:34 -0700

    fixed added code to pass subtraction test

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import unittest
 class TestCase (unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(example.add(1, 2), 3)
+
     def test_sub_1(self):
         self.assertEqual(example.sub(3, 2), 1)
 
