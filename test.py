390ede632d06ceee53a72f86c28767a739025156
Alanna <croysdaa@oregonstate.edu>
2021-06-06 22:16:29 -0700

    refixed linting errors

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import example
 import unittest
 
 
-class TestCase (unittest.TestCase):    
+class TestCase (unittest.TestCase):
     def test_add_1 (self):
-        self.assertEqual (example.add(1, 2), 3)
+        self.assertEqual (example.add (1, 2), 3)
 
 
 if __name__ == '__main__':
