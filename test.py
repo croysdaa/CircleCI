0e86200d74569f5a9790ab32bb71d1939b6c754c
Alanna <croysdaa@oregonstate.edu>
2021-06-06 22:05:16 -0700

    fixed linting errors

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ class TestCase (unittest.TestCase):
     def test_add_1 (self):
         self.assertEqual(example.add(1,2), 3)
 
-if __name__ == '__main__:
+if __name__ == '__main__':
     unitest.main()
